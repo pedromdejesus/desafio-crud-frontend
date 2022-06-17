2a14583e5bebf5ebc1c46246280a6d8ab5cc6a88
Jeej <jeejprincipal@gmail.com>
2022-06-17 10:42:11 -0300

    Criação de formulário de cripto moedas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import { Form } from './components/Form'
 
 function App() {
   return <div className="main">
     <div className="sub-main">
+      <Form />
     </div>
     <div className="sub-main">
     </div>
