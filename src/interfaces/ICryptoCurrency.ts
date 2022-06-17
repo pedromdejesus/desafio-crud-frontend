9c175a02dfe55cd8852d893c44a551b0fcde8a20
Jeej <jeejprincipal@gmail.com>
2022-06-17 10:36:47 -0300

    Criação de interface de cripto moedas ICryptoCurrency.

---
+++ src/interfaces/ICryptoCurrency.ts
@@ -0,0 +1,8 @@
+export interface ICryptoCurrency {
+    id?: number,
+    abbreviations: string,
+    description: string,
+    price: number,
+    inclusionDate?: string,
+    updateDate?: string
+}
