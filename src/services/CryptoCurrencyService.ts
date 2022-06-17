20e8674559e9bb338dd4a325e0225f7d2d911165
Jeej <jeejprincipal@gmail.com>
2022-06-17 10:37:51 -0300

    Criação de serviço CryptoCurrencyService.

---
+++ src/services/CryptoCurrencyService.ts
@@ -0,0 +1,36 @@
+
+import { ICryptoCurrency } from "../interfaces/ICryptoCurrency";
+import { api } from "./api/apiConfig";
+
+const getAll = async (): Promise<ICryptoCurrency[]> => {
+    const { data } = await api().get("/cryptocurrencies");
+    return data;
+};
+
+const post = async (cryptoCurrency: ICryptoCurrency): Promise<ICryptoCurrency> => {
+    const { data } = await api().post("/cryptocurrencies", cryptoCurrency);
+    return data;
+};
+
+const put = async (cryptoCurrency: ICryptoCurrency): Promise<ICryptoCurrency> => {
+    const { data } = await api().post("/cryptocurrencies", cryptoCurrency);
+    return data;
+};
+
+const deleteById = async (id: number): Promise<number> => {
+    const { data } = await api().delete(`/cryptocurrencies/${id}`);
+    return data;
+};
+
+const search = async (keyword: string): Promise<ICryptoCurrency[]> => {
+    const { data } = await api().get(`/cryptocurrencies/search/?keyword=${keyword}`);
+    return data;
+};
+
+export const CryptoCurrenciesService = {
+    getAll,
+    post,
+    put,
+    deleteById,
+    search
+};
